ff608b60850bc849ba86d30b284c0d1cba60adfd
bilal <bilalalam173@gmail.com>
2020-12-24 19:13:50 +0500

    improved tests

--- test/priorityQueue.test.js
+++ test/priorityQueue.test.js
@@ -9,7 +9,7 @@ test('should add values with integer and float keys', () => {
     const pque = new PriorityQueue();
     const inputs = ranArray(100, pque);
 
-    expect(pque.peek()).toEqual(min(inputs));
+    expect(pque.top).toEqual(min(inputs));
     expect(pque.length).toEqual(100);
 });
 
@@ -18,15 +18,15 @@ test('should pop and rearrange correctly everytime', () => {
     const pque = new PriorityQueue();
     const inputs = ranArray(100, pque);
     inputs.sort((a, b) => a - b);
-    expect(pque.peek()).toEqual(inputs[0]);
+    expect(pque.top).toEqual(inputs[0]);
     pque.pop();
     inputs.shift();
     inputs.sort((a, b) => a - b);
-    expect(pque.peek()).toEqual(inputs[0]);
+    expect(pque.top).toEqual(inputs[0]);
     pque.pop();
     inputs.shift();
     inputs.sort((a, b) => a - b);
-    expect(pque.peek()).toEqual(inputs[0]);
+    expect(pque.top).toEqual(inputs[0]);
 });
 
 test('should pop when only one element too', () => {
@@ -41,7 +41,7 @@ test('should add very large amount of keys in 1 second', () => {
     const t1 = Date.now();
     const inputs = ranArray(100000, pque);
     const t2 = Date.now();
-    expect(pque.peek()).toEqual(min(inputs));
+    expect(pque.top).toEqual(min(inputs));
     expect(pque.length).toEqual(100000);
     expect((t2 - t1) / 1000).toBeLessThanOrEqual(1);
 
